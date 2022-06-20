44a1fd40456ad2ed6cd13195f79b6a410cdf6d5b
Jose Villanueva <lv428694@gmail.com>
2022-06-19 21:42:27 -0500

    update: form to place create

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Place;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,8 @@ class PlaceController extends Controller
      */
     public function create()
     {
-        return view('places.create');
+        $categories = Category::all();
+        return view('places.create', compact('categories'));
     }
 
     /**
--- resources/views/places/create.blade.php
+++ resources/views/places/create.blade.php
@@ -1,5 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-    vdfd
+    <div class="container">
+        <h1 class="text-center mt-4"> Registrar establecimiento</h1>
+
+        <div class="mt-5 row justify-content-center">
+            <form action="" class="col-md-9 col-xs-8 card card-body">
+                <fieldset class="border p-4">
+                    <legend class="text-primary">Completa la información de tu establecimiento</legend>
+                    <div class="form-group">
+                        <label for="name">Nombre de establecimiento</label>
+                        <input type="text" id="name" class="form-control m-2 @error('name') is-invalid @enderror"
+                               placeholder="Ingrese nombre de establecimiento" name="name" value="{{old('name')}}">
+
+                        @error('name')
+                            <div class="invalid-feeback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Categoría</label>
+                        <select name="category_id" id="category" class="form-control m-2 @error('category') is-invalid  @enderror">
+                            <option value="" selected disabled>-- Seleccione una categoría --</option>
+                            @foreach($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                        @error('category')
+                        <div class="invalid-feeback">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Imagen</label>
+                        <input type="file" id="image" class="form-control m-2 @error('image') is-invalid @enderror"
+                               name="image" value="{{old('image')}}">
+
+                        @error('image')
+                        <div class="invalid-feeback">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="category">País</label>
+                        <select name="country_id" id="country" class="form-control m-2 @error('country') is-invalid  @enderror">
+                            <option value="" selected disabled>-- Seleccione un país --</option>
+                            <option value="1">México</option>
+                        </select>
+                        @error('country')
+                        <div class="invalid-feeback">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Dirección</label>
+                        <input type="text" id="address" class="form-control m-2"
+                               placeholder="Ingrese dirección">
+                        <p class="text-secondary mt-5 mb-3 text-center">Se añadirá en el mapa un aproximado de la dirrección ingresada</p>
+                    </div>
+                </fieldset>
+
+            </form>
+        </div>
+    </div>
 @endsection
